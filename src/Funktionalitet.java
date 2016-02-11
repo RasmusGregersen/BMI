3c09130b11a32d9d71a832781bf0018142e2841c
Hannibal Moulvad <Hannibal Moulvad>
2016-02-11 17:19:25 +0100

    Slettet autogenerated

--- src/Funktionalitet.java
+++ src/Funktionalitet.java
@@ -3,19 +3,16 @@ public class Funktionalitet implements IFunktionalitet {
 
 	@Override
 	public double getBMI(String cpr) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public String getTextualBMI(String cpr) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getNavn(String cpr) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
