febe5f0ef08a9b2b9af77387d1f396615b1b2293
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-02-11 16:38:06 +0100

    Data Class created

---
+++ src/Data.java
@@ -0,0 +1,37 @@
+public class Data implements IData{
+	private class Person {
+		String cpr;
+		String navn;
+		double hoejde;
+		double vaegt;
+		public Person(String cpr, String navn, double hoejde, double vaegt) {
+			super();
+			this.cpr = cpr;
+			this.navn = navn;
+			this.hoejde = hoejde;
+			this.vaegt = vaegt;
+		}
+	}
+	private ArrayList<Person> personer;
+	public Data(){
+		personer = new ArrayList<Person>();
+		// Tilføj personer
+		personer.add(new Person("234567-8901", "Ib Olsen", 1.80, 75.0));
+		personer.add(new Person("456789-0123", "Ole Jensen", 1.75, 95.0));
+		personer.add(new Person("123456-7890", "Eva Hansen", 1.65, 65.0)); }
+	public String getNavn(String cpr){ //Returner personens navn
+		for (int i= 0; i< personer.size(); i++)
+			if (personer.get(i).cpr.equals(cpr))
+				return personer.get(i).navn;
+		return null; }
+	public double getVaegt(String cpr){ //Returner personens vægt
+		for (int i= 0; i< personer.size(); i++)
+			if (personer.get(i).cpr.equals(cpr))
+				return personer.get(i).vaegt;
+		return -1; }
+	public double getHoejde(String cpr){ //Returner personens højde
+		for (int i= 0; i< personer.size(); i++)
+			if (personer.get(i).cpr.equals(cpr))
+				return personer.get(i).hoejde;
+		return -1; }
+}
