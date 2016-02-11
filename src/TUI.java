bc98c61c3a525f79458457f12f60ce07198d3cc8
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-02-11 18:56:37 +0100

    Removed implements in TUI class

--- src/TUI.java
+++ src/TUI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TUI implements IFunktionalitet {
+public class TUI {
 	public static void main(String[] args) {
 		IFunktionalitet f = new Funktionalitet();
 		Scanner scan = new Scanner (System.in);
@@ -9,17 +9,5 @@ public class TUI implements IFunktionalitet {
 		System.out.println(f.getNavn(userinput)); //234567-8901
 		System.out.println(Math.ceil(f.getBMI(userinput)));
 		System.out.println(f.getTextualBMI(userinput));
-	}
-
-	public double getBMI(String cpr) {
-		return 0;
-	}
-
-	public String getTextualBMI(String cpr) {
-		return null;
-	}
-
-	public String getNavn(String cpr) {
-		return getNavn(cpr);
 	}
 }
