bf200b251ab8f4b424892f181ba295a1979f8dc2
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-02-11 19:08:40 +0100

    TUI updated

--- src/TUI.java
+++ src/TUI.java
@@ -6,8 +6,9 @@ public class TUI {
 		
 		System.out.println("Indtast CPR nummer");
 		String userinput = scan.nextLine();
-		System.out.println(f.getNavn(userinput)); //234567-8901
-		System.out.println(Math.ceil(f.getBMI(userinput)));
-		System.out.println(f.getTextualBMI(userinput));
+		System.out.println(f.getNavn(userinput) + " du har en BMI på: " + Math.ceil(f.getBMI(userinput)) + " - " + f.getTextualBMI(userinput));
+		//123456-7890
+		//234567-8901
+		//456789-0123
 	}
 }
